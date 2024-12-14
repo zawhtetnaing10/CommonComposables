a9ce37faaebe20851f14804b523816b5572406ee
Zaw Htet Naing <zawgyi.gog@gmail.com>
2024-12-14 11:18:40 +0630

    Implemented annotated string

--- app/src/main/java/com/zg/commoncomposables/ui/composables/TextDemo.kt
+++ app/src/main/java/com/zg/commoncomposables/ui/composables/TextDemo.kt
@@ -7,9 +7,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,7 +20,23 @@ import com.zg.commoncomposables.ui.theme.CommonComposablesTheme
 
 @Composable
 fun TextDemo(modifier: Modifier = Modifier) {
+    // Building Annotated Text
+    val annotatedText = buildAnnotatedString {
+        withStyle(style = SpanStyle(color = Color.Magenta, fontSize = 24.sp)){
+            append("First Text")
+        }
+
+        withStyle(style = SpanStyle(color = Color.Green, fontSize = 32.sp, fontWeight = FontWeight.Bold)){
+            append("Second Text")
+        }
+
+        withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 12.sp, fontWeight = FontWeight.Bold)){
+            append("Third Text")
+        }
+    }
+
     Column(modifier = modifier) {
+        // Normal Text
         Text(
             "This is a very long text. This is a very long text. This is a very long text. This is a very long text. This is a very long text. ",
             fontSize = 24.sp,
@@ -31,6 +50,9 @@ fun TextDemo(modifier: Modifier = Modifier) {
             textDecoration = TextDecoration.LineThrough,
             modifier = Modifier.background(color = Color.Cyan).padding(8.dp)
         )
+
+        // Annotated Text
+        Text(annotatedText)
     }
 }
 
