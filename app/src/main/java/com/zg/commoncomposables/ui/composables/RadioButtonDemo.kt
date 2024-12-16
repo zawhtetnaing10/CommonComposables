aa6e0b6c646e9906490be95a6a191022f90d9159
Zaw Htet Naing <zawgyi.gog@gmail.com>
2024-12-16 22:22:10 +0630

    Unnecessary commits removed

--- app/src/main/java/com/zg/commoncomposables/ui/composables/RadioButtonDemo.kt
+++ app/src/main/java/com/zg/commoncomposables/ui/composables/RadioButtonDemo.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
